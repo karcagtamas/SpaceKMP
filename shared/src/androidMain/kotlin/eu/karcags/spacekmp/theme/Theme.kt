4c4b5bd3e04f110a319173dac2dec96f9e4981bb
Karcag Tamas <karcagtamas@outlook.com>
2026-08-27 23:47:13 +0200

    Resources

--- shared/src/androidMain/kotlin/eu/karcags/spacekmp/theme/Theme.kt
+++ shared/src/androidMain/kotlin/eu/karcags/spacekmp/theme/Theme.kt
@@ -1,5 +1,6 @@
 package eu.karcags.spacekmp.theme
 
+import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
--- shared/src/androidMain/kotlin/eu/karcags/spacekmp/theme/Type.kt
+++ shared/src/androidMain/kotlin/eu/karcags/spacekmp/theme/Type.kt
@@ -4,6 +4,7 @@ import androidx.compose.material3.Typography
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.googlefonts.Font
 import androidx.compose.ui.text.googlefonts.GoogleFont
+import eu.karcags.spacekmp.shared.R
 
 val provider = GoogleFont.Provider(
     providerAuthority = "com.google.android.gms.fonts",
